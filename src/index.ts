70f97c903ae3161f5634a55c8934db9e67868972
Fahim2023 <fahimshahrier2023@gmail.com>
2026-05-22 19:50:29 +0600

    just started the array's with typescript & saw some funciton's of tsconfigs

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-let sales: number = 123_456_789;
-let course: string = "Typescript";
-let is_published: boolean = true;
-let level;
-level = 1;
-level = "a";
-level = "b";
+// let sales: number = 123_456_789;
+// let course: string = "Typescript";
+// let is_published: boolean = true;
+// let level;
+// level = 1;
+// level = "a";
 
-function render(document) {
-  console.log(document);
-}
+// function render(document) {
+//   console.log(document);
+// }
+let numbers = [1, 2, "3"];
