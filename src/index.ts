034191b6405933b0dae340807a6a6aaacdf34e31
Fahim2023 <fahimshahrier2023@gmail.com>
2026-05-23 17:00:51 +0600

    just coding the array's with typescript

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,9 @@
 // function render(document) {
 //   console.log(document);
 // }
-let numbers = [1, 2, "3"];
+// let numbers: number[] = [1, 2, "3"];
+// let numbers: number[] = [1, 2, 3];
+// let numbers: number[] = [];
+// numbers[0] = 1;
+// numbers[1] = "1";
+// numbers.forEach(n=>n.)
